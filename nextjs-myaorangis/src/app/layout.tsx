10f3e85c008f74227fceb5cf6c22956f997a4755
Cindy Orangis <cindyledev@gmail.com>
2026-04-09 11:26:13 -0400

    add header and footer

--- nextjs-myaorangis/src/app/layout.tsx
+++ nextjs-myaorangis/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 const nunito = Nunito({
   subsets: ["latin"],
   variable: "--font-nunito",
@@ -22,7 +25,11 @@ export default function RootLayout({
       <body
         className={`${nunito.variable} font-[family-name:var(--font-nunito)] antialiased`}
       >
-        {children}
+        <div>
+          <Header />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
